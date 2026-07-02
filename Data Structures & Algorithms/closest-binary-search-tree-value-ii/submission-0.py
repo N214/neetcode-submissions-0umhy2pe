2d24291c8fc455f6204d4b108a3372fc77704a1c
N214 <34739662+N214@users.noreply.github.com>
2026-07-02 00:22:47 -0400

    Add: closest-binary-search-tree-value-ii - submission-0

---
+++ Data Structures & Algorithms/closest-binary-search-tree-value-ii/submission-0.py
@@ -0,0 +1,13 @@
+class Solution:
+    def closestKValues(self, root: Optional[TreeNode], target: float, k: int) -> List[int]:
+        def dfs(node, arr):
+            if not node:
+                return
+            arr.append(node.val)
+            dfs(node.left, arr)
+            dfs(node.right, arr)
+
+        arr = []
+        dfs(root, arr)
+        arr.sort(key= lambda x: abs(x-target))
+        return arr[:k]

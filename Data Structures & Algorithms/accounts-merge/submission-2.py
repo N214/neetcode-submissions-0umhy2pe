210b68f4d2f6dbd2b4c3d3272214c2a71f98ddcb
N214 <34739662+N214@users.noreply.github.com>
2026-08-08 23:14:44 -0400

    Add: accounts-merge - submission-2

---
+++ Data Structures & Algorithms/accounts-merge/submission-2.py
@@ -0,0 +1,32 @@
+from collections import defaultdict
+class Solution:
+    def accountsMerge(self, accounts: List[List[str]]) -> List[List[str]]:
+        graph = defaultdict(list)
+        for account in accounts:
+            first_email = account[1]
+            for e in account[2:]:
+                graph[first_email].append(e)
+                #print(f"graph key is {first_email} append {e}")
+                #print(graph)
+                graph[e].append(first_email)
+                #print(f"graph key is {e} append {first_email}")
+                #print(graph)
+        res = []
+        visited = set()
+
+        def dfs(node, emails):
+            visited.add(node)
+            emails.append(node)
+            for nei in graph[node]:
+                if not nei in visited:
+                    dfs(nei, emails)
+
+        for account in accounts:
+            for e in account[1:]:
+                if e not in visited:
+                    emails = []
+                    dfs(e, emails)
+                    name = account[0]
+                    res.append([name]+sorted(emails))
+        return res
+            
